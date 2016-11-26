c7cb55bd3b11d71dc64c85946d6df9d8d79fae01
Ben McGraw <mcgrue@verge-rpg.com>
2016-11-26 15:29:09 -0800

    Fix it for real this time ,_,

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ var request = require('request');
 
 if (process.env.REDISTOGO_URL) {
   var rtg   = require("url").parse(process.env.REDISTOGO_URL);
-  var redis = require("redis").createClient(rtg.port, rtg.hostname);
+  var redis = require("redis");
+  var client = redis.createClient(rtg.port, rtg.hostname);
 
-  redis.auth(rtg.auth.split(":")[1]);
+  client.auth(rtg.auth.split(":")[1]);
 } else {
-  var redis = require("redis").createClient();
+  var redis = require("redis");
+  var client = redis.createClient(); //creates a new client
 }
 
-var client = redis.createClient(); //creates a new client
+
 var cheerio = require('cheerio');
 var trim = require('trim');
 var pretty = require('js-object-pretty-print').pretty, address, value;
