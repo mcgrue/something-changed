f80012fe2482a64f6ff3c96ef6786f7f9d6142a5
Ben McGraw <mcgrue@verge-rpg.com>
2016-11-26 17:27:16 -0800

    WIP - these tests should fail because Phantom ignores CORS

--- index.js
+++ index.js
@@ -23,6 +23,11 @@ var pr2 = JSON.stringify;
 // Promise.promisifyAll(redis.RedisClient.prototype);
 // Promise.promisifyAll(redis.Multi.prototype);
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 
 app.set('port', (process.env.PORT || 5000));
 
